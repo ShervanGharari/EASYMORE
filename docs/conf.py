ee7f313041b7d301c992565d36d30521888d19ca
ShervanGharari <sh.gharari@gmail.com>
2023-08-27 17:45:05 -0600

    added the extentions and top directoryies

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,16 @@ author = 'Shervan Gharari'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# Extensions from conf.py
+extensions = [
+    'sphinx.ext.autodoc',	     # To generate autodocs
+    'sphinx.ext.mathjax',           # autodoc with maths
+    'sphinx.ext.napoleon'           # For auto-doc configuration
+]
+
+# paths from conf.py to top directory
+sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('.'))
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
