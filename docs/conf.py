caf1110512e633e8ef54c4b9b6b4991999669e79
ShervanGharari <sh.gharari@gmail.com>
2023-08-27 21:34:11 -0600

    adding the type of docstring to the config file

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+# paths from conf.py to top directory and where package is located
+import sys
+import os
+sys.path.insert(0, os.path.abspath('../src/easymore/'))
+
 project = 'EASYMORE'
 copyright = '2023, Shervan Gharari'
 author = 'Shervan Gharari'
@@ -20,15 +25,11 @@ extensions = [
     'sphinx.ext.napoleon'           # For auto-doc configuration
 ]
 
-# paths from conf.py to top directory
-import sys
-import os
-sys.path.insert(0, os.path.abspath('../src/easymore/'))
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
